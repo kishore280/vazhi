6bddad5cbc2d3aa70843d53f3875431b535bd6a4
kishore280 <maheshwarankishore@gmail.com>
2026-09-04 18:24:35 +0530

    agent router lets gooooo

--- backend/server/main.py
+++ backend/server/main.py
@@ -4,8 +4,10 @@ from fastapi import FastAPI
 
 sys.path.insert(0, "package")
 
-from server.routers import system_router
+from server.routers import agent_router, system_router
 
 app = FastAPI()
 
 app.include_router(system_router.router)
+app.include_router(agent_router.router)
+
